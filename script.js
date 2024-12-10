06712eba6d166ce0635dfc5bd68a8a0134db5ccb
reciproca1 <reciproca71@gmail.com>
2024-12-10 21:00:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿// Инициализация Swiper
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
